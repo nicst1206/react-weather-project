e9122d55d2e26ebb606406abe029ba319a85a2db
nicst1206 <nicole@rust-frankfurt.de>
2025-01-09 17:20:59 +0100

    fixed map issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -35,6 +35,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay forecast={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
