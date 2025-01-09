9a1e4a8101a0d43aa0f39661e4980f957ca47968
nicst1206 <nicole@rust-frankfurt.de>
2025-01-09 16:41:49 +0100

    added functionality for API Forecast in Display for one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  function displayData(response) {
-    console.log(response);
-  }
-  let longitude = props.data.coordinates.longitude;
-  let latitude = props.data.coordinates.latitude;
-  let apiKey = "obbt00a19b0447e3fbfabf89040f3c88";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayData);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-Weekday">Thu</div>
-          <img src={props.data.emoji} alt={props.data.alternate} />
-          <div>
-            <span className="WeatherForecast-Temp-max">12°C</span>
-            <span className="WeatherForecast-Temp-min">4°C</span>
+  function displayData(response) {
+    setLoaded(true);
+    setForecast(response.data);
+  }
+
+  function searchForecast() {
+    let longitude = props.data.coordinates.longitude;
+    let latitude = props.data.coordinates.latitude;
+    let apiKey = "obbt00a19b0447e3fbfabf89040f3c88";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayData);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay forecast={forecast} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    searchForecast();
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  console.log(props);
+  console.log(props.forecast.city);
+  function day() {
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let date = new Date(props.forecast.daily[0].time * 1000);
+    let day = date.getDay();
+    return days[day];
+  }
+
+  function temperatureMax() {
+    let temperature = Math.round(props.forecast.daily[0].temperature.maximum);
+    return `${temperature}°C`;
+  }
+
+  function temperatureMin() {
+    let temperature = Math.round(props.forecast.daily[0].temperature.minimum);
+    return `${temperature}°C`;
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="WeatherForecast-Weekday">{day()}</div>
+      <img
+        src={props.forecast.daily[0].condition.icon_url}
+        alt={props.forecast.daily[0].condition.description}
+      />
+      <div>
+        <span className="WeatherForecast-Temp-max">{temperatureMax()}</span>
+        <span className="WeatherForecast-Temp-min">{temperatureMin()}</span>
+      </div>
+    </div>
+  );
+}
